e3caf2a2fcdf121c88e116a273ef242094145598
Shadarien Williams <shadarien@shadwilliams.dev>
2025-02-19 08:13:45 -0500

    Implemented method overloading (using the same function name but takes different data types as parameters)

--- Calculator.java
+++ Calculator.java
@@ -1,17 +1,52 @@
+import java.util.ArrayList;
+import java.lang.Math;
+
 public class Calculator{
 
     public static float add (float num1, float num2){
         return num1 + num2;
     }
 
+    public static float add (ArrayList<Float> numbers){
+        float answer =0;
+        for (int i=0; i<numbers.size(); i ++){
+            answer = answer + numbers.get(i);
+        }
+        return answer;
+    }
+
     public static float subtract ( float num1, float num2){
-        
+        return num1 - num2;
+    }
+
+    public static float subtract (ArrayList<Float>numbers){
+        float answer = 0;
+        // must be set to the first number provided, or else everything will be negative regardless of the first number
+        answer = numbers.get(0);
+        // we start at 1 since we already have a preset answer above
+        for(int i=1; i < numbers.size(); i++){
+            answer = answer - numbers.get(i);
+        }
+        return answer;
+
     }
 
     public static float multiply ( float num1, float num2){
         return num1 * num2;
     }
+    public static float multiply (ArrayList<Float>numbers){
+        float answer = 0;
+        // must be set to the first number provided, or else everything will be negative regardless of the first number
+        answer = numbers.get(0);
+        // we start at 1 since we already have a preset answer above
+        for(int i=1; i < numbers.size(); i++){
+            answer = answer * numbers.get(i);
+            }
+        return answer;
 
+    }
+
+    
     public static float divide (float num1, float num2){
         return num1 / num2;
     }
@@ -24,5 +59,10 @@ public class Calculator{
         return answer;
     }
 
+    // Scientific Calculator functions 
+    // TODO: Add the ability to use scientific calculator functions such as log,cos,and tangent 
+    public static void logger(double valueHere){
+        System.out.println(Math.log(valueHere));
+    }
 
 }
--- test.java
+++ test.java
@@ -1,8 +1,20 @@
+import java.util.ArrayList;
+
 public class test{
     public static void main (String[] args){
         System.out.println("Calculator started");
         float answer = Calculator.exponential(2,3);
+
+        ArrayList<Float> tests = new ArrayList<Float>();
+        tests.add(2.0f);
+        tests.add(2.0f);
+        tests.add(2.0f);
+        tests.add(2.0f);
+        tests.add(2.0f);
+
+        answer = Calculator.multiply(tests);
         System.out.println(answer);
+
 
     }
 }
